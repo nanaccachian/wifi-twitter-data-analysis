981363b0939a051bbcf65188129d277f6bf6100e
Nicolas Armen Naccachian <nanaccachian@gmail.com>
2021-08-11 12:49:47 -0300

    Update TRIGGER TWEETS TABLE.sql

--- Proyecto final/T-SQL/TRIGGER TWEETS TABLE.sql
+++ Proyecto final/T-SQL/TRIGGER TWEETS TABLE.sql
@@ -1,4 +1,4 @@
-CREATE TRIGGER dbo.raw_tweets_to_DW ON dbo.raw_tweets AFTER INSERT
+CREATE TRIGGER [dbo].[raw_tweets_to_DW] ON [dbo].[raw_tweets] AFTER INSERT
 AS
 
 INSERT INTO dbo.dim_users 
@@ -28,8 +28,6 @@ INSERT INTO dbo.dim_users
 	FROM inserted 
 	WHERE tw_rt_user_id NOT IN (SELECT us_id FROM dbo.dim_users)
 
-
-
 INSERT INTO dbo.dim_datetimes
 	SELECT 
 		CONVERT(DECIMAL(4,0),SUBSTRING(tw_created_at,27,4)),
@@ -39,7 +37,7 @@ INSERT INTO dbo.dim_datetimes
 	FROM inserted
 	WHERE 
 		LTRIM(STR(CAST(SUBSTRING(tw_created_at,9,2) AS INT)))+CONVERT(NVARCHAR(2),dbo.month_to_num(SUBSTRING(tw_created_at,5,3)))+SUBSTRING(tw_created_at,27,4)+SUBSTRING(tw_created_at,12,8)	 
-		NOT IN (SELECT LTRIM(STR(ti_day))+LTRIM(STR(ti_month))+LTRIM(STR(ti_day))+CONVERT(NVARCHAR(8),ti_time) FROM dbo.dim_datetimes)
+		NOT IN (SELECT LTRIM(STR(ti_day))+LTRIM(STR(ti_month))+LTRIM(STR(ti_year))+CONVERT(NVARCHAR(8),ti_time) FROM dbo.dim_datetimes)
 
 INSERT INTO dbo.dim_datetimes 
 	SELECT 
@@ -50,7 +48,7 @@ INSERT INTO dbo.dim_datetimes
 	FROM inserted
 	WHERE 
 		LTRIM(STR(CAST(SUBSTRING(tw_qu_created_at,9,2) AS INT)))+CONVERT(NVARCHAR(2),dbo.month_to_num(SUBSTRING(tw_qu_created_at,5,3)))+SUBSTRING(tw_qu_created_at,27,4)+SUBSTRING(tw_qu_created_at,12,8)	 
-		NOT IN (SELECT LTRIM(STR(ti_day))+LTRIM(STR(ti_month))+LTRIM(STR(ti_day))+CONVERT(NVARCHAR(8),ti_time) FROM dbo.dim_datetimes)
+		NOT IN (SELECT LTRIM(STR(ti_day))+LTRIM(STR(ti_month))+LTRIM(STR(ti_year))+CONVERT(NVARCHAR(8),ti_time) FROM dbo.dim_datetimes)
 
 INSERT INTO dbo.dim_datetimes 
 	SELECT 
@@ -61,13 +59,13 @@ INSERT INTO dbo.dim_datetimes
 	FROM inserted
 	WHERE 
 		LTRIM(STR(CAST(SUBSTRING(tw_rt_created_at,9,2) AS INT)))+CONVERT(NVARCHAR(2),dbo.month_to_num(SUBSTRING(tw_rt_created_at,5,3)))+SUBSTRING(tw_rt_created_at,27,4)+SUBSTRING(tw_rt_created_at,12,8)	 
-		NOT IN (SELECT LTRIM(STR(ti_day))+LTRIM(STR(ti_month))+LTRIM(STR(ti_day))+CONVERT(NVARCHAR(8),ti_time) FROM dbo.dim_datetimes)
+		NOT IN (SELECT LTRIM(STR(ti_day))+LTRIM(STR(ti_month))+LTRIM(STR(ti_year))+CONVERT(NVARCHAR(8),ti_time) FROM dbo.dim_datetimes)
 
 
 INSERT INTO dbo.fact_tweets (tw_id,tw_created_at,tw_text,tw_user)
 	SELECT
 		tw_id,
-		(SELECT ti_id FROM dbo.dim_datetimes 
+		(SELECT TOP 1 ti_id FROM dbo.dim_datetimes 
 			WHERE CAST(SUBSTRING(tw_created_at,9,2) AS INT) = ti_day 
 					AND dbo.month_to_num(SUBSTRING(tw_created_at,5,3)) = ti_month
 					AND CAST(SUBSTRING(tw_created_at,27,4) AS INT) = ti_year
@@ -75,13 +73,13 @@ INSERT INTO dbo.fact_tweets (tw_id,tw_created_at,tw_text,tw_user)
 		CASE WHEN tw_truncated = 'false' THEN tw_text ELSE tw_full_text END,
 		tw_user_id
 	FROM inserted
-	WHERE tw_reply_id <> ''
+	WHERE (tw_reply_id = '' OR tw_reply_id IS NULL) AND (tw_qu_id = '' OR tw_qu_id IS NULL) AND (tw_rt_id = '' OR tw_rt_id IS NULL)
 
 /* INSERTA EN TWEETS LOS QUOTED TWEETS) */
 INSERT INTO dbo.fact_tweets (tw_id,tw_created_at,tw_text,tw_user)
 	SELECT
 		tw_qu_id,
-		(SELECT ti_id FROM dbo.dim_datetimes 
+		(SELECT TOP 1 ti_id FROM dbo.dim_datetimes 
 			WHERE CAST(SUBSTRING(tw_qu_created_at,9,2) AS INT) = ti_day 
 					AND dbo.month_to_num(SUBSTRING(tw_qu_created_at,5,3)) = ti_month
 					AND CAST(SUBSTRING(tw_qu_created_at,27,4) AS INT) = ti_year
@@ -95,7 +93,7 @@ INSERT INTO dbo.fact_tweets (tw_id,tw_created_at,tw_text,tw_user)
 INSERT INTO dbo.fact_tweets (tw_id,tw_created_at,tw_text,tw_user)
 	SELECT
 		tw_rt_id,
-		(SELECT ti_id FROM dbo.dim_datetimes 
+		(SELECT TOP 1 ti_id FROM dbo.dim_datetimes 
 			WHERE CAST(SUBSTRING(tw_rt_created_at,9,2) AS INT) = ti_day 
 					AND dbo.month_to_num(SUBSTRING(tw_rt_created_at,5,3)) = ti_month
 					AND CAST(SUBSTRING(tw_rt_created_at,27,4) AS INT) = ti_year
@@ -109,7 +107,7 @@ INSERT INTO dbo.fact_replies (rp_id,rp_tweet_id,rp_created_at,rp_text,rp_user)
 	SELECT
 		tw_id,
 		tw_reply_id,
-		(SELECT ti_id FROM dbo.dim_datetimes 
+		(SELECT TOP 1 ti_id FROM dbo.dim_datetimes 
 			WHERE CAST(SUBSTRING(tw_created_at,9,2) AS INT) = ti_day 
 					AND dbo.month_to_num(SUBSTRING(tw_created_at,5,3)) = ti_month
 					AND CAST(SUBSTRING(tw_created_at,27,4) AS INT) = ti_year
@@ -117,13 +115,13 @@ INSERT INTO dbo.fact_replies (rp_id,rp_tweet_id,rp_created_at,rp_text,rp_user)
 		CASE WHEN tw_truncated = 'false' THEN tw_text ELSE tw_full_text END,
 		tw_user_id
 	FROM inserted
-	WHERE tw_rt_id <> ''
+	WHERE tw_reply_id <> ''
 
 INSERT INTO dbo.fact_retweets (rt_id,rt_tweet_id,rt_created_at,rt_user)
 	SELECT
 		tw_id,
 		tw_rt_id,
-		(SELECT ti_id FROM dbo.dim_datetimes 
+		(SELECT TOP 1 ti_id FROM dbo.dim_datetimes 
 			WHERE CAST(SUBSTRING(tw_created_at,9,2) AS INT) = ti_day 
 					AND dbo.month_to_num(SUBSTRING(tw_created_at,5,3)) = ti_month
 					AND CAST(SUBSTRING(tw_created_at,27,4) AS INT) = ti_year
@@ -138,7 +136,7 @@ INSERT INTO dbo.fact_quotes (qu_id,qu_tweet_id,qu_text,qu_user,qu_created_at)
 		tw_qu_id,
 		CASE WHEN tw_truncated = 'false' THEN tw_text ELSE tw_full_text END,
 		tw_user_id,
-		(SELECT ti_id FROM dbo.dim_datetimes 
+		(SELECT TOP 1 ti_id FROM dbo.dim_datetimes 
 			WHERE CAST(SUBSTRING(tw_created_at,9,2) AS INT) = ti_day 
 					AND dbo.month_to_num(SUBSTRING(tw_created_at,5,3)) = ti_month
 					AND CAST(SUBSTRING(tw_created_at,27,4) AS INT) = ti_year
