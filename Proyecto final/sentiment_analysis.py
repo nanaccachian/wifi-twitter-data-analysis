b92b04ccb4fea31da31d2b39ec39bcfb580207fa
Nicolas <nanaccachian@gmail.com>
2021-08-17 11:30:42 -0300

    Dataframes creados

--- Proyecto final/sentiment_analysis.py
+++ Proyecto final/sentiment_analysis.py
@@ -6,6 +6,18 @@ import pandas as pd
 server = 'hxsqldev02\sql2016'
 database = 'Academy_Dev_Data_RFlor'
 driver = 'SQL Server'
-engine = sal.create_engine(f'mssql://{server}/{database}?driver={driver}')
-
+engine = create_engine(f'mssql+pyodbc://{server}/{database}?driver={driver}')
 conn = engine.connect()
+
+df_replies = pd.read_sql_query('SELECT * FROM fact_quotes', engine)
+print(df_replies.describe())
+df_quotes = pd.read_sql_query('SELECT * FROM fact_retweets', engine)
+print(df_quotes.describe())
+df_retweets = pd.read_sql_query('SELECT * FROM fact_replies', engine)
+print(df_retweets.describe())
+df_tweets = pd.read_sql_query('SELECT * FROM fact_tweets', engine)
+print(df_tweets.describe())
+
+
+
+
